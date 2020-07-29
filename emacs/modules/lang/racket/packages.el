6ed445c25af8b7b1157805bd9f9346736b46f26d
Ryder Stanley <ryderstanley2007@gmail.com>
2020-07-29 16:59:49 -0600

    got rid of bad file

--- emacs/modules/lang/racket/packages.el
+++
@@ -1,4 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; lang/racket/packages.el
-
-(package! racket-mode :pin "29afd2544a2f14bf29f3e542f6177579d0ae581f")
--- emacs/modules/ui/tabs/packages.el
+++
@@ -1,4 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; ui/tabs/packages.el
-
-(package! centaur-tabs :pin "9114ef423e820eee3f9c6cb29e112fc22aee84dd")
